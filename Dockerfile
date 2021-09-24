db4f95df195def9e0c2a07431904dad56ae64f4c
rob9315 <dev.robk@gmail.com>
2021-09-24 20:14:46 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM alpine
 COPY target/release/rust_redirect rust_redirect
-EXPOSE 443
+EXPOSE 80/tcp
 CMD rust_redirect
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use std::{
     env::var,
     net::{Ipv4Addr, SocketAddr},
-    str::FromStr,
+    // str::FromStr,
 };
 use warp::{hyper::Uri, Filter};
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::any().map(|| {
-        warp::redirect(
-            Uri::from_str(&match var("REDIR") {
+    let hello = warp::any().map(move || {
+        warp::redirect::temporary(
+            Uri::try_from(&match var("REDIR") {
                 Ok(s) => s,
-                Err(_) => "https://next-gen.dev".to_owned(),
+                Err(_) => "https://www.next-gen.dev".to_owned(),
             })
             .unwrap(),
         )
     });
-
     for addr in get_ports() {
         warp::serve(hello).run(addr).await;
     }
@@ -28,9 +27,9 @@ fn get_ports() -> Vec<SocketAddr> {
             .split(",")
             .map(|string| string.to_string().parse::<u16>().unwrap())
             .collect::<Vec<u16>>(),
-        Err(_) => vec![80, 443],
+        Err(_) => vec![80],
     }
     .into_iter()
     .map(|port| SocketAddr::from((Ipv4Addr::from([127, 0, 0, 1]), port)))
-    .collect::<Vec<SocketAddr>>()
+    .collect()
 }
